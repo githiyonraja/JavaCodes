c196a6415df308cb03b0728b098d29658f68616b
Githiyon <githi001@gmail.com>
2025-03-29 03:57:47 +0530

    String basic question Skip a character

---
+++ Recursion/SkipALetter.java
@@ -0,0 +1,25 @@
+package Recursion;
+
+public class SkipALetter {
+    public static void main(String[] args) {
+        String word = "abccbacd";
+        char letter = 'c';
+        String up = "";
+        skip(up,word,letter);
+    }
+
+    static void skip(String p, String up, char letter){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+
+        char ch = up.charAt(0);
+
+        if(ch == letter){
+            skip(p,up.substring(1),letter);
+        }else{
+            skip(p+ch, up.substring(1),letter);
+        }
+    }
+}
